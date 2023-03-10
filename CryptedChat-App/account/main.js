a2c057abc7ca411820feb00fd46255af42fc61c2
krisi <125066650+kristin-lina-todorova@users.noreply.github.com>
2023-03-10 15:22:25 +0200

    Profile ui

---
+++ CryptedChat-App/account/main.js
@@ -0,0 +1,52 @@
+function setFromMessage(formElement, type, message) {
+    const messageElement = formElement.querySelector(".form__message");
+
+    messageElement.textContent = message;
+    messageElement.classLisy.remove("form__message--success", "form__message--error");
+    messageElement.classList.add('form__message==${type}');
+}
+
+function setInputError(inputElement, message) {
+    inputElement.classList.add("form__input--error");
+    inputElement.parentElement.querySelector(".form__input-error=message").textContent = message;
+}
+
+function clearInputError(inputElement) {
+    inputElement.classlist.remove("form__input--error");
+    inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#createAccount");
+
+    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.add("form--hidden");
+        createAccountForm.classList.remove("form--hidden");
+    });
+
+    document.querySelector("#linkLogin").addEventListener("click", e => {
+        e.prevemtDefault();
+        loginForm.classList.remove("form--hiddem");
+        createAccountForm.classList.add("form--hidden");
+    });
+    loginForm.addEventListener("submit", e => {
+        e.preventDefault();
+        //Perform your AJAX/Fetch login
+
+        setFormMessage(loginForm, "error", "Invalid username/password");
+    });
+
+    document.querySelectorAll("form__input").forEach(inputElement => {
+        inputElement.addEventListener("blur", e => {
+            if(e.target.id==="signupUsername"&& e.target.value.length > 0 && e.target.value.lemght < 10){
+                setInputError(inputElement, "Username must be at least 10 chracters in lenght")
+    }});
+
+
+        inputElement.addEventListener("input", e => {
+            clearInputError(inputElement);
+        });
+    });
+});
